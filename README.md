98f4e277fc1362c048790609c43285dc1cb39d88
Nilesh <nilesh@haachico.com>
2022-12-12 17:38:52 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Dr-Ambedkar-Quiz.
---
+++ script.js
@@ -0,0 +1,4 @@
+const question = document.querySelector(".question");
+const answer = document.querySelector(".answer");
+const submitButton = querySelector(".submit-btn");
+const output = querySelector(".output");
